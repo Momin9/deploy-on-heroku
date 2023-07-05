d292c051cb0ff8f307a6e44497cb53810f0095bc
Momin Ali <mominalikhoker589@gmail.com>
2023-07-05 17:59:37 +0500

    create super user api

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,9 +2,11 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer, RegisterSerializer
-from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
 # Class based view to Get User Details using Token Authentication
@@ -22,3 +24,16 @@ class UserDetailAPI(APIView):
 class RegisterUserAPIView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+@api_view(['POST'])
+def create_superuser(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email')
+
+    if not username or not password or not email:
+        return Response({'error': 'Missing required fields'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_superuser(username=username, password=password, email=email)
+    return Response({'success': 'Superuser created'}, status=status.HTTP_201_CREATED)
